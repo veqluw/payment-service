40ece47c18b257d4aa62af43681f2ec7cec53275
Mikhail <mikhailveqluw@gmail.com>
2026-01-19 14:18:48 +0300

    add document collection payments

---
+++ src/main/java/practical/task/paymentservice/model/Payment.java
@@ -0,0 +1,23 @@
+package practical.task.paymentservice.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
+
+@Document(collection = "payments")
+@Data
+public class Payment {
+
+    @Id
+    private String id;
+
+    private String orderId;
+    private String userId;
+    private String status;
+    private Integer paymentAmount;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+}
