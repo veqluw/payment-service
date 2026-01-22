885672e056e860af1d63cefa01d543c62e73edf8
Mikhail <mikhailveqluw@gmail.com>
2026-01-22 21:03:11 +0300

    add payment controller

---
+++ src/main/java/practical/task/paymentservice/controller/PaymentController.java
@@ -0,0 +1,43 @@
+package practical.task.paymentservice.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import practical.task.paymentservice.dto.PaymentCreateDto;
+import practical.task.paymentservice.dto.PaymentResponseDto;
+import practical.task.paymentservice.service.PaymentService;
+
+import java.util.List;
+
+@RestController("/payments")
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @Autowired
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<PaymentResponseDto> create(@Valid @RequestBody PaymentCreateDto paymentCreateDto) {
+        PaymentResponseDto paymentResponseDto = paymentService.create(paymentCreateDto);
+        return ResponseEntity.ok(paymentResponseDto);
+    }
+
+    @GetMapping("/by-user")
+    public List<PaymentResponseDto> getByUserId(@RequestParam String userId) {
+        return paymentService.getByUserId(userId);
+    }
+
+    @GetMapping("/by-order")
+    public List<PaymentResponseDto> getByOrderId(@RequestParam String orderId) {
+        return paymentService.getByUserId(orderId);
+    }
+
+    @GetMapping("/by-status")
+    public List<PaymentResponseDto> getByStatus(@RequestParam String status) {
+        return paymentService.getByUserId(status.toUpperCase());
+    }
+}
